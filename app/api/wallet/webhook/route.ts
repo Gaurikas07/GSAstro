4cff7f67597cc6e62da8665c46f42215ed6a37ae
Gaurikas07 <gaurikas54@gmail.com>
2026-02-18 22:57:16 +0530

    Update route.ts

--- app/api/wallet/webhook/route.ts
+++ app/api/wallet/webhook/route.ts
@@ -3,6 +3,7 @@ import { verifyWebhookSignature } from "@/lib/razorpay";
 import { connectDB } from "@/lib/db";
 import User from "@/lib/models/User";
 import Transaction from "@/lib/models/Transaction";
+export const runtime = 'nodejs';
 
 export async function POST(req: Request) {
   try {
