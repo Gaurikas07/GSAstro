4ad1deec07880a9233e08339ff5989f902249dea
Gaurikas07 <gaurikas54@gmail.com>
2026-02-19 13:00:11 +0530

    Update route.ts

--- app/api/admin/users/route.ts
+++ app/api/admin/users/route.ts
@@ -2,6 +2,7 @@ import { NextResponse } from 'next/server';
 import { connectDB } from '@/lib/db';
 import User from '@/lib/models/User';
 export const runtime = 'nodejs';
+export const dynamic = "force-dynamic";
 
 export async function GET() {
   await connectDB();
