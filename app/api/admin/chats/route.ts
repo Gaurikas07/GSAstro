c497bc774a7cf267751006b9f3ec8ce918a1a3b9
Gaurikas07 <gaurikas54@gmail.com>
2026-02-19 12:59:58 +0530

    Update route.ts

--- app/api/admin/chats/route.ts
+++ app/api/admin/chats/route.ts
@@ -2,6 +2,7 @@ import { NextResponse } from 'next/server';
 import { connectDB } from '@/lib/db';
 import Chat from '@/lib/models/Chat';
 export const runtime = 'nodejs';
+export const dynamic = "force-dynamic";
 
 export async function GET() {
   await connectDB();
